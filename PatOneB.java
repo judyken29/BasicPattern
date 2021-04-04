fcc13f6433a9a93d06ebe4340b259e18ef47408b
judyken29 <81622421+judyken29@users.noreply.github.com>
2021-04-04 19:51:39 +0530

    Create PatOneB.java

---
+++ PatOneB.java
@@ -0,0 +1,21 @@
+package pat;
+
+
+import java.util.Scanner;
+public class PatOneB {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the number of rows:");
+		int n=sc.nextInt();
+		int c=1;
+		for (int i=1; i<=n; i++)
+		{
+			for (int j=1; j<=i; j++)
+			{
+				System.out.print(c+" ");
+				c++;
+			}
+		System.out.print("\n");
+		}
+	}
+}
